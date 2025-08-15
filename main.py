3528dbabf052c15737e572074d47873d1f88c30a
Tyreese2070 <sc23t2g@leeds.ac.uk>
2025-08-15 17:58:31 +0100

    added code to load mnist dataset

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import tensorflow as tf
 
 learn = tf.contrib.learn
 tf.logging.set_verbosity(tf.logging.ERROR)
+
+# Load mnist data set
+mnist = learn.datasets.load_dataset("mnist")
+data = mnist.train.images
+labels = np.asarray(mnist.train.labels, dtype=np.int32)
+test_data = mnist.test.images
+test_labels = np.asarray(mnist.test.labels, dtype=np.int32)
