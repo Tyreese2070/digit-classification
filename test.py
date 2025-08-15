f7dd9324bf22128d00c895b68c3d9f1f798dbeef
Tyreese2070 <sc23t2g@leeds.ac.uk>
2025-08-15 19:59:28 +0100

    Added save for mlp model and added test for predicting custom digits

--- test.py
+++ test.py
@@ -1,9 +1,26 @@
-import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import sys
 
-def display_digit(image, label, flattened=True):
-    if flattened:
-        image = image.reshape(28, 28)
-    plt.imshow(image, cmap='gray')
-    plt.title(f"Label: {label}")
-    plt.axis('off')
-    plt.show()
+model = tf.keras.models.load_model("digit_mlp_classifier.h5")
+
+if len(sys.argv) < 2:
+    print("Usage: python test.py <image_path>")
+    sys.exit(1)
+    
+image_path = sys.argv[1]
+
+img = Image.open(image_path).convert('L')  # Convert to grayscale
+img = img.resize((28, 28))  # Resize to 28x28
+
+img_array = np.array(img)
+img_array = 255-img_array  # Invert colors
+img_array = img_array.astype("float32") / 255.0  # Normalise pixel values
+
+img_array = np.expand_dims(img_array, axis=0)  # shape (1,28,28)
+
+pred_probability = model.predict(img_array)
+pred_label = np.argmax(pred_probability)
+
+print(f"Predicted label: {pred_label}"  )
